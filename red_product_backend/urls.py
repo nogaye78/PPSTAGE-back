db8cc2e55ad10ee96b6007328120d186f57fe286
Nogaye Ndao <nogayendao2020@gmail.com>
2026-01-23 11:23:13 +0000

    Fix accounts app module error

--- red_product_backend/urls.py
+++ red_product_backend/urls.py
@@ -1,22 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenObtainPairView
-from accounts.views import RegisterView
-from rest_framework.routers import DefaultRouter
-from hotels.views import HotelViewSet
-
-router = DefaultRouter()
-router.register(r'hotels', HotelViewSet)
+from django.urls import path, include # N'oublie pas d'importer 'include'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/register/', RegisterView.as_view()),
-    path('api/login/', TokenObtainPairView.as_view()), # C'est ici que tu récupères ton JWT
-    path('api/', include(router.urls)),
-] 
-
-# Ajout crucial pour les images
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/', include('accounts.urls')), # Django va chercher dans accounts/urls.py
+]
