3084c3397758c5bccee2e6caf03a65531ccb0041
Nogaye Ndao <nogayendao2020@gmail.com>
2026-01-24 20:59:11 +0000

    test

--- hotels/models.py
+++ hotels/models.py
@@ -1,9 +1,11 @@
+# hotels/models.py
 from django.db import models
-from django.contrib.auth.models import User
 
 class Hotel(models.Model):
     name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='hotels/', null=True, blank=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.URLField()
+
+    def __str__(self):
+        return self.name
